8bb8fe94afeb880e970305e09a4ba6a5f43d64f0
Bhavyasree-200666 <oletibhavyasree625@gmail.com>
2026-02-07 13:30:31 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,12 @@
+public class primitive{
+    public static void main(String[]args){
+        int tel=88;
+        int eng=95;
+        int math=85;
+        int sum=(tel+eng+math);
+        int avg=sum/3;
+        System.out.println(avg);
+        System.out.println(sum);
+    }
+
+}
